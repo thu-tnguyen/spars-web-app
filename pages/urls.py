652ba55883afd262e0aa298ad26146718bcd562b
Thu Nguyen <minhthu.nfl@gmail.com>
2019-03-30 21:14:40 -0700

    Update

--- pages/urls.py
+++ pages/urls.py
@@ -4,10 +4,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    #path('', views.HomePageView.as_view(), name='home'),
-    #path('about/', views.AboutPageView.as_view(), name='about'),
-    #url(r'^result/$', views.search, name='result'),
-    #path('projects/<uuid:ext_id>/', views.ProjectPageView.as_view(), name='rsprojects'),
-    #path('alumni_tree/', views.AlumniTreePageView.as_view(), name='alumni_tree'),
-    #path('contact/', views.ContactPageView.as_view(), name='contact'),
+    path('', views.HomePageView.as_view(), name='home'),
+    path('about/', views.AboutPageView.as_view(), name='about'),
+    url(r'^result/$', views.search, name='result'),
+    path('projects/<uuid:ext_id>/', views.ProjectPageView.as_view(), name='rsprojects'),
+    path('alumni_tree/', views.AlumniTreePageView.as_view(), name='alumni_tree'),
+    path('contact/', views.ContactPageView.as_view(), name='contact'),
 ] 
