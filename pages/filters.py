bed4fddfb6646062ec264904355eac831cd38733
Thu Nguyen <minhthu.nfl@gmail.com>
2019-04-26 08:55:51 -0700

    Removed filters.py

--- pages/filters.py
+++
@@ -1,9 +0,0 @@
-import django_filters
-from pages.models import Project,Author
-
-
-class ResultListFilter(django_filters.FilterSet):
-  class Meta:
-    model = Project
-    fields = ['title', 'division', 'mentor', 'year', 'presentation_format',] 
-    order_by = ['pk']
